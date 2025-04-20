3760eba5bdb690ae7f5c2c2be046d0fe26195b88
Stefaan <safwbdev@gmail.com>
2025-04-20 21:37:56 +0800

    update User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
     },
     designation: {
-        type: String,
+        type: [String],
         default: 'NA',
     },
     email: {
